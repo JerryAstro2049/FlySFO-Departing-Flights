cdd79aa6d1c08668c4d7f7f1f120ce3cfb6562a3
mindswapper2049 <jerryloi@163.com>
2020-11-26 15:25:34 +0800

    add flight item

--- Crawler/Crawler/items.py
+++ Crawler/Crawler/items.py
@@ -10,3 +10,14 @@ class CrawlerItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
     pass
+
+
+class FlightItem(scrapy.Item):
+    Airline = scrapy.Field()
+    DepartingTo = scrapy.Field()
+    Flight = scrapy.Field()
+    Scheduled = scrapy.Field()
+    Estimated = scrapy.Field()
+    Remarks = scrapy.Field()
+    Terminal = scrapy.Field()
+    Gate = scrapy.Field()
