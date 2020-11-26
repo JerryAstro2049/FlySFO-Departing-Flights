2f3da1f2cb5af9c1c74a59acfc9db27f48c55740
mindswapper2049 <jerryloi@163.com>
2020-11-26 15:34:58 +0800

    implement & enable flights to json pipeline

--- Crawler/Crawler/pipelines.py
+++ Crawler/Crawler/pipelines.py
@@ -7,7 +7,20 @@
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
 
+import json
+
 
 class CrawlerPipeline:
+    def __init__(self):
+        self.file = open('SFO today departs.json', 'w', encoding='utf-8')
+
     def process_item(self, item, spider):
+        line = "\n" + json.dumps(dict(item), ensure_ascii=False) + ","
+        self.file.write(line)
         return item
+
+    def open_spider(self, spider):
+        self.file.write('{"SFO today departs":[')
+
+    def close_spider(self, spider):
+        self.file.write('\n]}')
